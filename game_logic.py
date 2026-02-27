fb7a92364727e9a10ba9c620f0765bde2f1ef29a
Anete Kupča <kupcaanete@gmail.com>
2026-02-27 12:39:05 +0200

    izveidoju skeletiņu pēc apraksta

---
+++ game_logic.py
@@ -0,0 +1,12 @@
+class GameState:
+    def __init__(self):
+        pass
+
+    def generate_moves(self):
+        pass
+
+    def apply_move(self, move):
+        pass
+
+    def check_winner(self):
+        pass
--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-#kods ies šeit
+from game_logic import GameState
+
+def main():
+    game = GameState()
+
+if __name__ == "__main__":
+    main()
