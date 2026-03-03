45cdcc158849dd8fb86d1df4dbb6e612403399bd
Daniels <daniels.kitoks@edu.rtu.lv>
2026-03-03 14:46:14 +0200

    Izveidoju pilno spēles loģiku un moni testu lai pārbaudītu.

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from game_logic import GameState
+from game_logic import GameState, generate_sequence
 
 def main():
-    game = GameState()
+    seq = generate_sequence(15, seed=1)
+    state = GameState(seq)
+
+    print("Sākums:", state.numbers, "Punkti:", state.get_points(), "Turn:", state.turn)
+
+    # testam: vienkārši vienmēr izvēlas pirmo iespējamo gājienu
+    while not state.is_empty():
+        move = state.generate_moves()[0]
+        state = state.apply_move(move)
+
+    print("Beigas:", state.numbers, "Punkti:", state.get_points(), "Winner:", state.check_winner())
 
 if __name__ == "__main__":
     main()
