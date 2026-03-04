ef4878d590f3ecc970f4197c08a9fc1f5a682284
Anete Kupča <kupcaanete@gmail.com>
2026-03-04 11:44:57 +0200

    izveidoju apply_move funkciju

--- game_logic.py
+++ game_logic.py
@@ -13,7 +13,38 @@ class GameState:
         pass
 
     def apply_move(self, move):
-        pass
+        new_numbers = list(self.numbers)
+        new_p1 = self.p1_points
+        new_p2 = self.p2_points
+        move_type, value = move
+
+        if move_type == 'take':
+            new_numbers.remove(value)
+            if self.turn == 1:
+                new_p1 += value
+            else:
+                new_p2 += value
+        elif move_type == 'split':
+            if value == 2:
+                new_numbers.remove(2)
+                new_numbers.extend([1, 1])
+            elif value == 4:
+                new_numbers.remove(4)
+                new_numbers.extend([2, 2])
+                if self.turn == 1:
+                    new_p1 += 1
+                else:
+                    new_p2 += 1
+        
+        child_node = GameState(
+            numbers=new_numbers,
+            p1_points=new_p1,
+            p2_points=new_p2,
+            turn=3 - self.turn, # 3-1 -> pirmais spēlētājs, 3-2 -> 2. spēlētājs
+            parent=self,
+            move_made=move
+        )
+        return child_node
 
     def check_winner(self):
         pass
