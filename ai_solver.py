d53872b6a9dfb3cb532bde1ebcd0dc792c65e56f
ChrissunGit <kristerssunne2005@gmail.com>
2026-03-15 17:16:36 +0200

    Pievienoju Tomasa grūtības izvēli, pievienoju minimax bez ab, pievienoju taimeri un node counteri, salaboju case _

--- ai_solver.py
+++ ai_solver.py
@@ -1,6 +1,11 @@
 import random
+import time
+
+nodes_generated = 0
+nodes_evaluated = 0
 
 def minimax_ab(state, depth, alpha, beta, is_maximizing):
+    global nodes_generated, nodes_evaluated
 
     """
         Rekursīva funkcija, kas aprēķina labāko iespējamo vērtību dotajai pozīcijai.
@@ -9,11 +14,13 @@ def minimax_ab(state, depth, alpha, beta, is_maximizing):
     """
     
     if state.is_empty() or depth == 0:
+        nodes_evaluated += 1
         return state.p1_points - state.p2_points
 
     if is_maximizing:
         max_eval = -float('inf')
         for move in state.generate_moves():
+            nodes_generated += 1
             child_state = state.apply_move(move)
             eval = minimax_ab(child_state, depth - 1, alpha, beta, False)
             max_eval = max(max_eval, eval)
@@ -24,6 +31,7 @@ def minimax_ab(state, depth, alpha, beta, is_maximizing):
     else:
         min_eval = float('inf')
         for move in state.generate_moves():
+            nodes_generated += 1
             child_state = state.apply_move(move)
             eval = minimax_ab(child_state, depth - 1, alpha, beta, True)
             min_eval = min(min_eval, eval)
@@ -33,12 +41,15 @@ def minimax_ab(state, depth, alpha, beta, is_maximizing):
         return min_eval
     
 def inv_minimax_ab(state, depth, alpha, beta, is_maximizing):   
+    global nodes_generated, nodes_evaluated
     if state.is_empty() or depth == 0:
+        nodes_evaluated += 1
         return state.p2_points - state.p1_points
 
     if is_maximizing:
         max_eval = -float('inf')
         for move in state.generate_moves():
+            nodes_generated += 1
             child_state = state.apply_move(move)
             eval = inv_minimax_ab(child_state, depth - 1, alpha, beta, False)
             max_eval = max(max_eval, eval)
@@ -49,6 +60,7 @@ def inv_minimax_ab(state, depth, alpha, beta, is_maximizing):
     else:
         min_eval = float('inf')
         for move in state.generate_moves():
+            nodes_generated += 1
             child_state = state.apply_move(move)
             eval = inv_minimax_ab(child_state, depth - 1, alpha, beta, True)
             min_eval = min(min_eval, eval)
@@ -57,12 +69,38 @@ def inv_minimax_ab(state, depth, alpha, beta, is_maximizing):
                 break 
         return min_eval
 
-def evaluate_position(state, depth, difficulty):
+def minimax(state, depth, is_maximizing):
+    global nodes_generated, nodes_evaluated
+    if state.is_empty() or depth == 0:
+        nodes_evaluated += 1
+        return state.p1_points - state.p2_points
+    if is_maximizing:
+        max_eval = -float('inf')
+        for move in state.generate_moves():
+            nodes_generated += 1
+            child_state = state.apply_move(move)
+            eval = minimax(child_state, depth - 1, False)
+            max_eval = max(max_eval, eval)
+        return max_eval
+    else:
+        min_eval = float('inf')
+        for move in state.generate_moves():
+            nodes_generated += 1
+            child_state = state.apply_move(move)
+            eval = minimax(child_state, depth - 1, True)
+            min_eval = min(min_eval, eval)
+        return min_eval
+    
+def evaluate_position(state, depth, movelist, move_index):
+    global nodes_generated, nodes_evaluated
 
     '''
         Atdod labāko kustību izmantojot minimax funkciju ar alpha-beta atcipršanas metodi 
     '''
+    nodes_generated = 1
+    nodes_evaluated = 0
 
+    start_time = time.time()
     best_move = None
     
     if state.turn == 1:
@@ -75,10 +113,11 @@ def evaluate_position(state, depth, difficulty):
     
     # Nākamo pāris kustību simulēšana izmantojot generate_moves un apply_move
     for move in state.generate_moves():
+        nodes_generated += 1
         child = state.apply_move(move)
         is_next_max = (child.turn == 1)
         
-        val = difficulty_changer(child,difficulty,alpha,beta,is_next_max,depth)
+        val = difficulty_changer(child,movelist,move_index,alpha,beta,is_next_max,depth)
             
         if state.turn == 1:
             if val > best_value:
@@ -88,10 +127,18 @@ def evaluate_position(state, depth, difficulty):
             if val < best_value:
                 best_value = val
                 best_move = move
-    return best_move
 
-def difficulty_changer(child, difficulty,alpha,beta,is_next_max, depth):
-    match difficulty:
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    return {
+        "move": best_move,
+        "time": elapsed_time,
+        "generated": nodes_generated,
+        "evaluated": nodes_evaluated
+    }
+
+def difficulty_changer(child, movelist, move_index,alpha,beta,is_next_max, depth):
+    match movelist[move_index]:
         case 0:
             val = minimax_ab(child, depth - 1, alpha, beta, is_next_max)
         case 1:
@@ -101,6 +148,31 @@ def difficulty_changer(child, difficulty,alpha,beta,is_next_max, depth):
                 val = inv_minimax_ab(child, depth - 1, alpha, beta, is_next_max)
         case 2:
             val = inv_minimax_ab(child, depth - 1, alpha, beta, is_next_max)
+        case 3:
+            val = minimax(child, depth -1, is_next_max)
         case _:
-            pass
+            val = minimax_ab(child, depth - 1, alpha, beta, is_next_max)
     return val
+
+def generate_list(difficulty, total_length):
+    desires_DB = [
+        [0.0, 1.0, 0.0],
+        [0.2, 0.6, 0.2],
+        [0.4, 0.2, 0.4],
+        [0.2, 0.1, 0.7],
+        [0.0, 0.0, 1.0],
+    ]
+    
+    ratios = desires_DB[difficulty-1]
+    movelist = []
+
+    count_inv = int(total_length * ratios[0])
+    count_ab = int(total_length * ratios[1])
+    count_pure = total_length - count_inv - count_ab
+    
+    movelist.extend([1] * count_inv)
+    movelist.extend([0] * count_ab)
+    movelist.extend([3] * count_pure)
+    
+    random.shuffle(movelist)
+    return movelist
--- main.py
+++ main.py
@@ -1,23 +1,48 @@
 from game_logic import GameState, generate_sequence
-from ai_solver import evaluate_position
+from ai_solver import evaluate_position, generate_list
 
 def main():
-    seq = generate_sequence(15, seed=1)
+    length = 15
+
+    seq = generate_sequence(length, seed=1)
     state = GameState(seq)
+
+    movelist = generate_list(5,length)
+
+    all_times = []
+    total_generated = 0
+    total_evaluated = 0
 
     print("Sākums:", state.numbers, "Punkti:", state.get_points(), "Turn:", state.turn)
 
+    move_index = 0
+
     while not state.is_empty():
-        move = evaluate_position(state, depth=4,difficulty=1)
+        current_idx = min(move_index, len(movelist) - 1)
+        result = evaluate_position(state, depth=4,movelist=movelist,move_index=current_idx)
+        move = result["move"]
         if move:
+            all_times.append(result["time"])
+            total_generated += result["generated"]
+            total_evaluated += result["evaluated"]
             state = state.apply_move(move)
+            move_index+=1
             p1, p2 = state.get_points()
             print(f"Gājiens: {move[0]} index {move[1]}")
             print(f"Virkne: {state.numbers} | Punkti: P1:{p1}, P2:{p2}")
+            print(f"--- Gājiena laiks: {result['time']:.4f}s | Virsotnes: {result['generated']}")
         else:
             break
 
-    print("Beigas:", state.numbers, "Punkti:", state.get_points(), "Winner:", state.check_winner())
+    avg_time = sum(all_times) / len(all_times) if all_times else 0
+
+    print("\nSPĒLES REZULTĀTI:")
+    print(f"Uzvarētājs: {state.check_winner()}")
+    print(f"Punkti: {state.get_points()}")
+    print(f"Vidējais laiks gājienam: {avg_time:.4f} s")
+    print(f"Kopā ģenerētās virsotnes: {total_generated}")
+    print(f"Kopā novērtētās virsotnes: {total_evaluated}\n")
+
     return state.check_winner()
 
 if __name__ == "__main__":
